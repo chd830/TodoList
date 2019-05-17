8e087d520834340b3f97eddffac88efdf9a4b71c
eunchong <chd830@naver.com>
2019-05-17 13:23:27 +0900

    제목, 우선순위 완료여부 테이블로 만들어서 출력

--- src/main/webapp/WEB-INF/resource/js/index.js
+++ src/main/webapp/WEB-INF/resource/js/index.js
@@ -1,3 +1,12 @@
 $.post("/getList", function (result) {
     console.dir(result);
+    for(var i = 0 in result) {
+        document.write("<table>")
+        document.write("<tr>")
+        document.write("<td>" + result[i].title + "</td>");
+        document.write("<td>" + result[i].priority + "</td>");
+        document.write("<td>" + result[i].isComplete + "</td>");
+        document.write("</tr>");
+        documentg.write("</table>");
+    }
 })
